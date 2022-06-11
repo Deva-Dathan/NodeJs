1a6cff1af219c91ac1fd3ce8adce2d221c0ccc52
Deva Dathan <78137542+Deva-Dathan@users.noreply.github.com>
2022-06-11 16:29:46 +0530

    alternate function declaration

--- sampleserver.js
+++ sampleserver.js
@@ -6,3 +6,15 @@ function server(req,res){
     res.write('Hello World')
     res.end()
 } 
+
+/* Function in the above code can be written in another ways */
+
+/* 
+
+var http=require("http")
+http.createServer(function(req,res){
+    res.write("Hello World")
+    res.end()
+}).listen(7000)
+
+*/
