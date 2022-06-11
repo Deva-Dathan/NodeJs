84bb051b8fb470c759a5bcb4b8e7780c4b07c272
Deva Dathan <78137542+Deva-Dathan@users.noreply.github.com>
2022-06-11 17:02:20 +0530

    add html file in server

--- sampleserver.js
+++ sampleserver.js
@@ -1,11 +1,15 @@
+/* Server Creating Code */
+/*
 var http=require('http')
 
 http.createServer(server).listen(7000)
 
-function server(req,res){
+function server(req,res){                   
     res.write('Hello World')
     res.end()
-} 
+} */
+
+/* To Kill the server press ctrl+c */
 
 /* Function in the above code can be written in another ways */
 
@@ -18,3 +22,15 @@ http.createServer(function(req,res){
 }).listen(7000)
 
 */
+
+
+var http=require('http')
+var fs=require('fs')
+
+http.createServer(function(req,res){
+    fs.readFile("samplehtml.html",function(error,data){
+        res.writeHead(200,{'Content-Type':'text/html'})
+        res.write(data)
+        res.end()
+    })
+}).listen(7000)
