22358d15721ab753064a2c9594e90bb89df26747
Deva Dathan <78137542+Deva-Dathan@users.noreply.github.com>
2022-06-14 23:55:25 +0530

    edited

--- sampleserver.js
+++ sampleserver.js
@@ -42,8 +42,9 @@ http.createServer(function(req,res){
 
 */
 
-/* creating url root page */
 
+/* creating url root page */
+/*
 var http=require('http')
 var fs=require('fs')
 
@@ -59,6 +60,37 @@ http.createServer(function(req,res){
     }else if(req.url==='/login'){
         res.write("Login Page")
         res.end()
+    }else{
+        res.write("ERROR")
+        res.end()
+    }
+
+
+}).listen(7000,()=>console.log("Server Started"))
+*/
+
+
+/*Adding html page to server root*/
+
+
+var http=require('http')
+var fs=require('fs')
+
+
+http.createServer(function(req,res){
+    
+    if(req.url==='/'){
+        fs.readFile('samplehtml.html',function(error,data){
+            res.writeHead(200,{'Content-Type':'text/html'})
+            res.write(data)
+            res.end()
+        })
+    }else if(req.url==='/signup'){
+        fs.readFile('sampleform.html',function(error,data){
+            res.writeHead(200,{'Content-Type':'text/html'})
+            res.write(data)
+            res.end()
+        })
     }else{
         res.write("ERROR")
         res.end()
