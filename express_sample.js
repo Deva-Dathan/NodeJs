b65832e2ccfffe2de6ebc1e9d0205d2ee08ef20a
Deva Dathan <78137542+Deva-Dathan@users.noreply.github.com>
2022-06-16 15:31:11 +0530

    modified express_sample file

--- express_sample.js
+++ express_sample.js
@@ -10,10 +10,25 @@ app.listen(3000,()=>console.log('Server Started'))
 
 
 /* Program of adding html page through express code */
+/*
+const express=require('express')
+const app=express()
+const path=require('path')
+
+app.get('/',(req,res)=>res.send('hello'))
+app.get('/signup',(req,res)=>res.sendFile(path.join(__dirname,'sampleform.html')))
+app.post('/signup',(req,res)=>res.send('Data Inserted'))  /* Passing data to server by post method */ /*
+app.get('/about',(req,res)=>res.send('About Page'))
+app.listen(3000,()=>console.log('Server Started'))
+
+
+/* use method in express */
+
 
 const express=require('express')
 const app=express()
 const path=require('path')
+app.use((req,res,next)=>{console.log('started');next()})    /* Mainly used for session,cookie,static webpage loading or logi authentication */
 
 app.get('/',(req,res)=>res.send('hello'))
 app.get('/signup',(req,res)=>res.sendFile(path.join(__dirname,'sampleform.html')))
