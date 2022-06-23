bcb06d75f5ecb0a9b88a8e53905365485e845179
Deva Dathan <78137542+Deva-Dathan@users.noreply.github.com>
2022-06-23 13:59:47 +0530

    promise example

--- promise_sample.js
+++ promise_sample.js
@@ -15,16 +15,54 @@ function div(num1,num2,callback){
     callback(num1/num2)
 }
 
-add(10,20,(sum,err)=>{
-    if(err){
-        console.log("Number is zero");
-    }else{
-        console.log("Sum Of Addition : "+sum);
-        multiply(sum,sum,(product)=>{
-            console.log("Product Of Multiplication : "+product)
-            div(product,10,(result)=>{
-                console.log("Quotient Of Division : "+result);
-            })
-        })
-    }
+
+const { resolve, reject } = require('promise')
+// add(10,20,(sum,err)=>{
+//     if(err){
+//         console.log("Number is zero");
+//     }else{
+//         console.log("Sum Of Addition : "+sum);
+//         multiply(sum,sum,(product)=>{
+//             console.log("Product Of Multiplication : "+product)
+//             div(product,10,(result)=>{
+//                 console.log("Quotient Of Division : "+result);
+//             })
+//         })
+//     }
+// })
+
+
+
+/* Build a prgm for adding 2 number,then multiply the sum and divide the product with 10 Using promise function*/
+
+const promise=require('promise')
+
+function add(num1,num2){
+    return new Promise((resolve,reject)=>{
+        if(num1==0){
+            reject("ERROR")
+        }
+        resolve(num1+num2)
+    })
+}
+function multiply(num1,num2){
+    return new Promise((resolve,reject)=>{
+        resolve(num1*num2)
+    })
+}
+function div(num1,num2){
+    return new Promise((resolve,reject)=>{
+        resolve(num1/num2)
+    })
+}
+add(10,20).then((sum)=>{
+    console.log(sum);
+    multiply(sum,sum).then((product)=>{
+        console.log(product);
+        div(product,10).then((result=>{
+            console.log(result);
+        }))
+    })
+}).catch((err)=>{
+    console.log(err);
 })
